0b71383c3c2bb368d1427fe03cc8148a3cb1ece9
CGI <CGI>
2019-03-06 10:17:51 +0530

    Sorting

---
+++ Bubble_sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 5, 7, 10, 6, 4, 3, 15, 20, 17, 11 };
+            BubbleSort(numbers);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + "\t");
+            }
+            Console.WriteLine("\n");
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        swap(numbers, i, j);
+                    }
+                }
+            }
+        }
+
+        private static void swap(int[] numbers, int i, int j)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = temp;
+        }
+    }
+}
---
+++ Selection_sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 5, 7, 10, 6, 4, 3, 15, 20, 17, 11 };
+            SelecionSort(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+                Console.Write(numbers[i] + "/t");
+        }
+
+        private static void SelecionSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int small_index = i;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[small_index] > numbers[j])
+                    {
+                        small_index = j;
+                    }
+                }
+                Swap(numbers, i, small_index);
+            }
+        }
+
+        private static void Swap(int[] numbers, int i, int small_index)
+        {
+            int temp = numbers[i];
+            numbers[i] = numbers[small_index];
+            numbers[small_index] = temp;
+        }
+    }
+}
